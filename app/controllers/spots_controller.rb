412cad86040e6fbd4b3ba892b971188f7acc0b6c
Kohji-Saitoh <koji.s102030@gmail.com>
2023-03-17 15:15:27 +0900

    controller作成

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,6 +1,14 @@
 class SpotsController < ApplicationController
-    def index
-        @q = User.ransack(params[:q])
-        @users = @q.result(distinct: true)
+    before_action :search
+
+    def search
+        # params[:q]のqには検索フォームに入力した値が入る
+        @q = Spot.ransack(params[:q])
     end
+
+    def index
+        # distinct: trueは重複したデータを除外
+        @spots = @q.result(distinct: true)
+    end
+    
 end
