eccc98909141b988a3254ffcc23a9427bcc61c23
Kohji-Saitoh <koji.s102030@gmail.com>
2023-03-20 11:47:19 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :spots do
-    collection do
-      get 'search'
-    end
-  end
+  get '/spots', to: 'spots#index'
 end
