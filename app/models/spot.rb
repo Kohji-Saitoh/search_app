ddb9ae1908b96d4e7c55fdab38036478cc976c2b
Kohji-Saitoh <koji.s102030@gmail.com>
2023-03-27 15:21:26 +0900

    スポットテーブルにバリデーション追加

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,4 +1,8 @@
 class Spot < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to :region
+    
+  belongs_to :region
+  validates :name, presence: true
+  validates :text, presence: true
+  validates :region_id, presence: true, numericality: {greater_than: 1}
 end
